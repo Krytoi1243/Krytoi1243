0cb2d7c3132bb334b8a7b2e553a46dd727611700
Krytoi1243 <98270653+Krytoi1243@users.noreply.github.com>
2022-02-20 18:45:35 +0300

    Add files via upload

---
+++ hjytuk.py
@@ -0,0 +1,114 @@
+
+"""Цикл For функция Range"""
+
+
+# number = 1, 2, 3, 4
+#
+# for i in numbers:
+#     print(i)
+
+# start = int(input('введите начальное число последовательности'))
+# stop = int(input('введите конечное число последовательности'))
+#
+#
+#
+# for number in range(start, stop):
+#     if number % 2 == 0:   #проверка на четность
+#       print(number)
+
+
+
+# start = int(input('введите начальное число последовательности: '))
+# stop = int(input('введите конечное число последовательности: '))
+#
+#
+# for number in range(stop, start, -1):
+#     if number % 5 == 0:
+#         if number % 10 == 0:
+#             continue
+#         print(number)
+
+
+# string = 'привет Тимофей!'
+#
+# for i in string:
+#     print(i)
+
+
+# start = int(input('введите начальное число последовательности: '))
+# stop = int(input('введите конечное число последовательности: '))
+#
+# otr = 0
+# pol = 0
+#
+# for number in range(start, stop):
+#    if number < 0:
+#     otr += number #otr = number + otr
+#    else:
+#     pol += number
+#
+#
+# start = int(input(f'сумма положительных чисел: {pol}'))
+# stop = int(input(f'сумма отрицательных чисел: {pol}'))
+
+
+# n = int(input('введи кол-во цветов радуги'))
+#
+# for i in range(1, n+1):
+#    if i == 1:
+#        print('красный')
+#    if i == 2:
+#        print('оранжевый')
+#    if i == 3:
+#        print('желтый')
+#    if i == 4:
+#        print('зеленый')
+#    if i == 5:
+#        print('голубой')
+#    if i == 6:
+#        print('синий')
+#    if i == 7:
+#        print('фиолетовый')
+#
+# if n > 7 or n < 1:
+#     print('радуга состоит только из семи цветов')
+
+
+
+# n = int(input('введите натуральное число: '))
+#
+#
+#  print(f'{n} x 1 = {n * 1}')
+#  print(f'{n} x 2 = {n * 2}')
+#  print(f'{n} x 3 = {n * 3}')
+#  print(f'{n} x 4 = {n * 4}')
+#  print(f'{n} x 5 = {n * 5}')
+#  print(f'{n} x 6 = {n * 6}')
+#  print(f'{n} x 7 = {n * 7}')
+#  print(f'{n} x 8 = {n * 8}')
+#  print(f'{n} x 9 = {n * 9}')
+#  print(f'{n} x 10 = {n * 10}')
+#
+#
+# for i in range(1, 11):
+#     print(f'{n} x {i} = {n * i}')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
